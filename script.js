89409626b0ec978796fd8105cb0faa533ca48739
Patrick <taaaallen@gmail.com>
2023-03-20 00:28:56 +0100

    Almost got the UI version working Still not playing within the loop. Not sure how to fix atm.

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
 let playerScore = 0;
 let computerScore = 0;
 let rounds = 0;
-
-let playerScoreText = document.querySelector("#player-score");
-let computerScoreText = document.querySelector("#computer-score");
-let roundsText = document.querySelector("#rounds");
+let playerScoreText = document.querySelector("#player-score span");
+let computerScoreText = document.querySelector("#computer-score span");
+let roundsText = document.querySelector("#rounds #count");
+let playerMoveText = document.querySelector("#player-move-text #player-move");
+let computerMoveText = document.querySelector("#computer-move-text #computer-move");
+let gameText = document.querySelector("#game-text");
 
 const resetGameBtn = document.querySelector("#reset-game");
 resetGameBtn.addEventListener("click", () => {
   resetGame();
 });
-
-// const choices = document.querySelectorAll("#game-area .choice");
-// choices.forEach((choice) => {
-//   choice.addEventListener("click", (e) => {
-//     console.log("Player choice: ", e.target.id);
-//     playRound(e.target.vale, getComputerChoice());
-//   });
-// });
 
 function resetGame() {
   playerScore = 0;
@@ -29,7 +23,7 @@ function resetGame() {
 function incrementRound() {
   rounds++;
   roundsText.innerText = rounds;
-  console.log("Rounds from incrementRound", rounds);
+  // console.log("Rounds from incrementRound", rounds);
 }
 
 function getComputerChoice() {
@@ -38,16 +32,12 @@ function getComputerChoice() {
   //   "Computer choice: ",
   //   choices[Math.floor(Math.random() * choices.length)]
   // );
-  return choices[Math.floor(Math.random() * choices.length)];
-}
-
-function getPlayerChoice(choice) {
-  choice.toLowerCase();
-  return choice;
+  const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  computerMoveText.innerText = `${computerChoice}`;
+  return computerChoice;
 }
 
 function playRound(playerMove, computerMove) {
-  incrementRound();
   //   console.log("Player move: ", playerMove, "\nComputer move: ", computerMove);
   if (!playerMove || !computerMove) {
     console.log(playerMove, computerMove);
@@ -55,23 +45,23 @@ function playRound(playerMove, computerMove) {
     return null;
   }
 
+  incrementRound();
+
   // Define rules outside of this?
   if (playerMove == computerMove) {
-    return "It's a tie!";
+    gameText.innerText = "It's a tie!";
   } else if (
     (playerMove == "rock" && computerMove == "scissors") ||
     (playerMove == "scissors" && computerMove == "paper") ||
     (playerMove == "paper" && computerMove == "rock")
   ) {
     playerScore++;
-    return `${capitalizeFirstLetter(
-      playerMove
-    )} beats ${computerMove}. You win!`;
+    playerScoreText.innerText = playerScore;
+    gameText.innerText = `${capitalizeFirstLetter(playerMove)} beats ${computerMove}. You win!`;
   } else {
     computerScore++;
-    return `${capitalizeFirstLetter(
-      computerMove
-    )} beats ${playerMove}. You lose!`;
+    computerScoreText.innerText = computerScore;
+    gameText.innerText = `${capitalizeFirstLetter(computerMove)} beats ${playerMove}. You lose!`;
   }
   // return the winning move and its player
 }
@@ -80,45 +70,36 @@ function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function rules() {
-  // Create rules for rock beats scissors etc.
-  // to easily call in playRound()?
-}
-
 function determineWinner() {
   if (playerScore >= 3 || (rounds == 5 && playerScore > computerScore)) {
     alert("You win");
-  } else if (
-    computerScore >= 3 ||
-    (rounds == 5 && computerScore > playerScore)
-  ) {
+  } else if (computerScore >= 3 || (rounds == 5 && computerScore > playerScore)) {
     alert("You lose");
   }
 }
 
+const choices = document.querySelectorAll("#game-area .choice");
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    console.log("Player choice: ", e.target.id);
+    playerMoveText.innerText = `${e.target.id}`;
+    playRound(e.target.id, getComputerChoice());
+  });
+});
+
 function game() {
   console.log("Game is live!");
   // let rounds = 0;
-  const choices = document.querySelectorAll("#game-area .choice");
+  console.log(rounds);
 
-  while (rounds < 5) {
-    // console.log("Rounds from game", rounds);
-    // const playerChoice = prompt("Rock, paper or scissors?");
-    // getPlayerChoice(playerChoice);
-    choices.forEach((choice) => {
-      choice.addEventListener("click", (e) => {
-        console.log("Player choice: ", e.target.id);
-        const playerMove = e.target.id;
-        const computerChoice = getComputerChoice();
-        console.log("Computer choice: ", computerChoice);
-        playRound(playerMove, computerChoice);
-      });
-    });
-    // rounds++;
+  for (let i = 0; i < 5; i++) {
+    console.log("Playing!");
+    console.log(rounds);
+
+    continue;
   }
   determineWinner();
   console.log("Game is over");
 }
 
-// console.log(playRound(playerChoice, computerChoice));
 game();
